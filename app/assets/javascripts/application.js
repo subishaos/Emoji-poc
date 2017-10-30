550078cebb4af1bc6dfef0e680314272804b73b1
subishaos <subisha@7nodes.com>
2017-10-30 12:04:29 +0530

    Emoji tone removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,31 +9,10 @@
 //= require emojionearea.min
 
 $(document).ready(function() {
-  $("#emojionearea1").emojioneArea({
-    pickerPosition: "right",
-    tonesStyle: "bullet"
-  });
-  $("#emojionearea2").emojioneArea({
+
+  $("#emojionearea").emojioneArea({
     pickerPosition: "bottom",
-    tonesStyle: "radio"
-  });
-  $("#emojionearea3").emojioneArea({
-    pickerPosition: "right",
-    filtersPosition: "bottom",
-    tonesStyle: "square"
-  });
-  $("#emojionearea4").emojioneArea({
-    pickerPosition: "bottom",
-    filtersPosition: "bottom",
-    tonesStyle: "checkbox"
-  });
-  $("#emojionearea5").emojioneArea({
-    pickerPosition: "top",
-    filtersPosition: "bottom",
     tones: false,
-    autocomplete: false,
-    inline: true,
-    hidePickerOnBlur: false
   });
   $("#standalone").emojioneArea({
     standalone: true,
